42407931504e158eff4b6f792caca4286d3b3e03
Gokavarapu, Sree Ganesh <sreeganesh.gokavarapu@ecolab.com>
2025-01-11 17:23:21 +0530

    updated controller name for Platforms

--- CommandService/Controllers/PlatformsController.cs
+++ CommandService/Controllers/PlatformsController.cs
@@ -4,9 +4,9 @@ namespace CommandService.Controllers
 {
     [Route("api/c/[controller]")]
     [ApiController]
-    public class PlatformController : ControllerBase
+    public class PlatformsController : ControllerBase
     {
-        public PlatformController()
+        public PlatformsController()
         {
             
         }
